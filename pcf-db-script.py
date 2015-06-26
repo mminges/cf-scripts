be8dd0a5763e9ad9ef505d040cb48c175e4bff73
mminges <mminges@ecsteam.com>
2015-06-26 07:37:44 -0600

    minor changes

--- pcf-db-script.py
+++ pcf-db-script.py
@@ -4,28 +4,20 @@ import datetime
 import time
 import argparse
 from datetime import date, timedelta
-from boto.exception import BotoServerError
 
 def getSnapshots():
     "Lists all snapshots that were created using this script"
 
+    print("\nListing all snapshots associated with [%s] ... " % (dbInstanceName))
     snapshotList = []
     for snapshot in dbSnapshots:
-        if snapshot.id.startswith(dbSnapshotBase) and snapshot.status == 'available':
+        if snapshot.id.startswith('tc-pcf-bosh-snapshot') and snapshot.status == 'available':
             snapshotList.append(snapshot.id)
     snapshotList.sort(reverse=True)
+    for name in snapshotList:
+        print("\n%s " % (name))
 
     return snapshotList
-
-def printSnapshots():
-    snapshots = getSnapshots()
-    if snapshots is None:
-        print("Couldn't find any snapshots for instance " + dbInstanceName + ", note name must start with " + dbSnapshotBase)
-        return
-    for name in snapshots:
-        print(name)
-
-    return None
 
 def getStatusOfDatabase():
     "Gets the status of the specified available database"
@@ -35,54 +27,41 @@ def getStatusOfDatabase():
 
     else:
         print("There is currently no available RDS database running. Please check the AWS Console if this is a problem!")
-
     return None
-
 
 def removeDatabase():
     snapshotName = dbSnapshotBase + '-' + today.strftime('%Y%m%d-%H%M')
-    print("Backing database up to snapshot name: %s " % snapshotName)
+    print("Before removing database, will create a snapshot under the following name: \n%s " % snapshotName)
 
-    deletedInstance = dbInstances[0].stop(skip_final_snapshot=False, final_snapshot_id=snapshotName)
 
-    iterationCount = 0
-    timerBreak = 30
 
-    print("Deleting database..." + dbInstanceName)
-    while (iterationCount < 40):
-        time.sleep(timerBreak)
-        try:
-            deletedInstance.update(validate=True)
-            deletedStatus = deletedInstance.status
-            print("deleted db status: " + deletedStatus)
-        except ValueError:
-            print("Could no longer access database status, assuming it has been deleted")
-            break
-        except BotoServerError as e:
-            if e.status == "404":
-                print("Could no longer access database status, assuming it has been deleted")
-            else:
-                print('Unknown botoServerError, giving up')
-                print('status=', e.status)
-                print('reason=', e.reason)
-                print('body=', e.body)
-                print('request_id=', e.request_id)
-                print('error_code=', e.error_code)
-                print('error_message=', e.error_message)
-            break
+    print("\nDatabase instances that are active are: %s " % (dbInstances))
+
+    # deletedInstance = dbInstances[0].stop(skip_final_snapshot = False, final_snapshot_id = snapshotName)
+    #
+    # iterationCount = 0
+    # timerBreak = 30
+    #
+    # while (iterationCount < 40):
+    #    time.sleep(timerBreak)
+    #    try:
+    #       deletedInstance.update(validate=True)
+    #       print ("deletion status: " + deletedInstance.status)
+    #    except ValueError:
+    #       print("could no longer access database status, assuming it has been deleted")
 
     return None
 
 def restoreDatabase():
 
     snapshots = getSnapshots()
-    if snapshots is None:
-        print("There are no snapshots to restore from, note name must start with " + dbSnapshotBase)
+    if snapshotList is None:
+        print("There are no snapshots to restore from")
         return
 
-    dbSnapshotName = snapshots[0]
     print("Will restore database from the most recent available snapshot: [%s] " % dbSnapshotName)
 
+    dbSnapshotName = snapshots[0]
     dbClassName = 'db.m3.large'
     secGroupId = 'sg-86020ce2'
     secGroupName = 'tc-mysql'
@@ -97,9 +76,10 @@ def restoreDatabase():
        try:
           restoredInstance.update(validate=True)
           restoredStatus = restoredInstance.status
-          print ("restored db status: " + restoredStatus)
+          print ("restored db status:  " + restoredStatus)
        except ValueError:
           print("could no longer access database status, exiting")
+
 
     conn.modify_dbinstance(dbInstanceName, vpc_security_groups=[secGroupId]);
 
@@ -138,7 +118,7 @@ elif args.restore:
     print("\nYour AWS RDS database is now restored!")
 
 elif args.get_snap:
-    printSnapshots()
+    getSnapshots()
 
 else:
     getStatusOfDatabase()
